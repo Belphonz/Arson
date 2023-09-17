20cd2d2798b62eea922217df5e65be6f1cc1fef4
Belphonz <belphonz@gmail.com>
2023-09-17 18:30:29 +0100

    Standard Class additions
    - Default Constructor
    - Copy Constructor
    - Move Constructor
    - Copy assignment
    - Operator= List assignment
    - Move assignment

--- include/Arson/Matrix.h
+++ include/Arson/Matrix.h
@@ -11,7 +11,7 @@ namespace Arn
 	{
 		const size_t _dimension_amount{sizeof...(Dimensions) + 1};
 		//Stores all dimensions size_wise
-		const size_t* _all_dimensions {new size_t[_dimension_amount]{Init_Dimension, Dimensions...}};
+		const size_t* _all_dimensions{new size_t[_dimension_amount]{Init_Dimension, Dimensions...}};
 
 		//Find and Return the size of the array
 		size_t array_size() const;
@@ -21,24 +21,35 @@ namespace Arn
 		T* _data{new T[_size]};
 
 	public:
+		//Default Constructor
+		Matrix() = default;
 		//Constructor 
 		Matrix(std::initializer_list<T> matrix);
+		//Copy Constructor
+		Matrix(const Matrix& delta_matrix);
+		//Move Constructor
+		Matrix(Matrix&& delta_matrix) noexcept;
+		//Destructor
+		~Matrix();
+
+		//Copy assignment 
+		Matrix& operator=(const Matrix& delta_matrix);
+		//Operator= List assignment   
+		Matrix& operator=(std::initializer_list<T> matrix);
+		//Move assignment
+		Matrix& operator=(Matrix&& delta_matrix) noexcept;
 
 		//Return the Element stored in the position given
 		//WARNING : Will throw an error if not the correct amount of indexes are given
-		template <size_t Index, size_t... Sup_Index >
+		template <size_t Index, size_t... Sup_Index>
 		T& at();
 
 		//Return the Element stored in the position given
 		//WARNING : Will throw an error if not the correct amount of indexes are given
 		//WARNING : Will throw an error if the index is outside dimension length
-		template <size_t Index, size_t... Sup_Index >
+		template <size_t Index, size_t... Sup_Index>
 		T& secure_at();
-
-		//Destructor
-		~Matrix();
 	};
-
 #include "Matrix_src.h"
 }
 
