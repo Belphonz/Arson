c6018752c3f42c3c73f2a207d72fa53a9aa974a5
Belphonz <belphonz@gmail.com>
2023-09-17 15:59:08 +0100

    Filled Matrix.h
    - Created Matrix_src.h to hold the Matrix source files
    - Constructor, Destructor ,
    And at (subscript operator replacement)

--- include/Arson/Matrix.h
+++ include/Arson/Matrix.h
@@ -1,10 +1,39 @@
-#pragma once
 #ifndef	ARSON_CLASS_MATRIX_H
 #define ARSON_CLASS_MATRIX_H
 
+#include <cstdarg>
+
 namespace Arn
 {
+	//Multidimensional array class of variable dimension, designed to optimize cache 
+	template <typename T, size_t Init_Dimension, size_t... Dimensions>
+	class Matrix
+	{
+		const size_t _dimension_amount{sizeof...(Dimensions) + 1};
+		//Stores all dimensions size_wise
+		const size_t* _all_dimensions {new size_t[_dimension_amount]{Init_Dimension, Dimensions...}};
 
+		//Find and Return the size of the array
+		size_t array_size() const;
+		const size_t _size{array_size()};
+
+		//Actual matrix
+		T* _data{new T[_size]};
+
+	public:
+		//Constructor 
+		Matrix(std::initializer_list<T> matrix);
+
+		//Return the Element stored in the position given
+		//WARNING : Will throw an error if not the correct amount of indexes are given
+		template <size_t Index, size_t... Sup_Index >
+		T& at();
+
+		//Destructor
+		~Matrix();
+	};
+
+#include "Matrix_src.h"
 }
 
 
---
+++ src/Matrix_src.h
@@ -0,0 +1,53 @@
+#ifndef	ARSON_CLASS_MATRIX_SRC_H
+#define ARSON_CLASS_MATRIX_SRC_H
+
+#include <cassert>
+
+template <typename T, size_t Init_Dimension, size_t ... Dimensions>
+size_t Matrix<T, Init_Dimension, Dimensions...>::array_size() const
+{
+	size_t total{1};
+	for (size_t index{0}; index < _dimension_amount; ++index)
+	{
+		total *= _all_dimensions[index];
+	}
+	return total;
+}
+
+template <typename T, size_t Init_Dimension, size_t ... Dimensions>
+Matrix<T, Init_Dimension, Dimensions...>::Matrix(std::initializer_list<T> matrix)
+{
+	assert("Too many elements given" && matrix.size() <= _size);
+	std::copy(matrix.begin(), matrix.end(), _data);
+}
+
+template <typename T, size_t Init_Dimension, size_t ... Dimensions>
+Matrix<T, Init_Dimension, Dimensions...>::~Matrix()
+{
+	delete [] _all_dimensions;
+	delete [] _data;
+}
+
+template <typename T, size_t Init_Dimension, size_t ... Dimensions>
+template <size_t Index, size_t ... Sup_Index>
+T& Matrix<T, Init_Dimension, Dimensions...>::at()
+{
+	assert("Incorrect ammount of elements given" && sizeof...(Sup_Index) + 1 == _dimension_amount);
+
+	const size_t* all_indexes{new size_t[_dimension_amount]{Index, Sup_Index...}};
+	size_t total{0};
+
+	//Calculation : x + max_x * y + max_x * max_y * z ...
+
+	for (size_t index{0}; index < _dimension_amount; ++index)
+	{
+		size_t current_index{all_indexes[index]};
+		for (size_t dimension{1}; dimension < index + 1; ++dimension)
+		{
+			current_index *= _all_dimensions[dimension - 1];
+		}
+		total += current_index;
+	}
+	return _data[total];
+}
+#endif
