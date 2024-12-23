d7d0a26606849e7a1784b8ea6dc1a06bd872475c
Aethel B. Crown <aethel.crown@gmail.com>
2024-12-23 20:18:33 +0000

    Modified the File Dialog
    - Changed Name of File Dialog function to specify it is Windows only
    - Added the ability to filter File types

--- include/Arson/Common/FileDialogue.h
+++ include/Arson/Common/FileDialogue.h
@@ -1,39 +1,52 @@
+#ifndef ARSON_FILE_DIALOGUE_H
+#define ARSON_FILE_DIALOGUE_H
+
 #include <windows.h>
 #include <shobjidl.h>
 
-std::wstring FileDialogue()
+namespace Arn
 {
-	HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
-		COINIT_DISABLE_OLE1DDE);
-	if (SUCCEEDED(hr))
+	//Opens a File Dialog and Returns a wstring
+	//fileFilters formatted as ' L"*.jpg;*.jpeg" '
+	std::wstring WindowsFileOpenDialog(LPCWSTR fileFilters = L"*")
 	{
-		IFileOpenDialog* pFileOpen;
-
-		// Create the FileOpenDialog object.
-		hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL,
-			IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
-
+		HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
+			COINIT_DISABLE_OLE1DDE);
 		if (SUCCEEDED(hr))
 		{
-			// Show the Open dialog box.
-			hr = pFileOpen->Show(NULL);
-			// Get the file name from the dialog box.
+			IFileOpenDialog* pFileOpen;
+
+			// Create the FileOpenDialog object.
+			hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL,
+				IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
+
+			COMDLG_FILTERSPEC rgSpec[] = { {L"", fileFilters} };
+			pFileOpen->SetFileTypes( 1,rgSpec );
+
 			if (SUCCEEDED(hr))
 			{
-				IShellItem* pItem;
-				hr = pFileOpen->GetResult(&pItem);
+				// Show the Open dialog box.
+				hr = pFileOpen->Show(NULL);
+				// Get the file name from the dialog box.
 				if (SUCCEEDED(hr))
 				{
-					PWSTR pszFilePath;
-					hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+					IShellItem* pItem;
+					hr = pFileOpen->GetResult(&pItem);
+					if (SUCCEEDED(hr))
+					{
+						PWSTR pszFilePath;
+						hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
 
-					pItem->Release();
-					return pszFilePath;
+						pItem->Release();
+						return pszFilePath;
+					}
 				}
+				pFileOpen->Release();
 			}
-			pFileOpen->Release();
+			CoUninitialize();
 		}
-		CoUninitialize();
+		return L"";
 	}
-	return L"";
 }
+
+#endif
